6f1e3fbda41891b527ac85f65fbe1623463785c3
Emiliano <emi@Asterix.local>
2015-01-09 00:50:04 -0300

    Chapter2: movimiento simple

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -12,6 +12,11 @@ class GameScene: SKScene {
 
     let zombie = SKSpriteNode(imageNamed: "zombie1")
     
+    var lastUpdateTime: NSTimeInterval = 0
+    var dt: NSTimeInterval = 0
+    let zombieMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPointZero
+    
     override func didMoveToView(view: SKView) {
         backgroundColor  = SKColor.whiteColor()
         let background = SKSpriteNode(imageNamed: "background1")
@@ -21,8 +26,53 @@ class GameScene: SKScene {
         
         zombie.position = CGPoint(x: 400, y: 400)
         addChild(zombie)
-        
 
     }
     
+    override func update(currentTime: NSTimeInterval) {
+        //zombie.position = CGPoint(x: zombie.position.x + 4, y: zombie.position.y)
+        moveSprite(zombie, velocity: velocity)
+        
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        println("\(dt*1000) milliseconds since last update")
+        
+    }
+    
+    func moveSprite(sprite: SKSpriteNode, velocity: CGPoint) {
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
+        println("Amount to move: \(amountToMove)")
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
+    }
+    
+    func moveZombieToward(location: CGPoint) {
+        //vector
+        let offset = CGPoint(x: location.x - zombie.position.x, y: location.y - zombie.position.y)
+        //largo del vector
+        let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
+        //normalizacion del vector (vector de largo 1)
+        let direction = CGPoint(x: offset.x / CGFloat(length), y: offset.y / CGFloat(length))
+        //para calcular el momento del vector multiplico el normalizado por la velocidad que quiero
+        velocity = CGPoint(x: direction.x * zombieMovePointsPerSec, y: direction.y * zombieMovePointsPerSec)
+    }
+    
+    func sceneTouched(touchLocation: CGPoint) {
+        moveZombieToward(touchLocation)
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        let touch = touches.anyObject() as UITouch
+        let touchLocation = touch.locationInNode(self)
+        sceneTouched(touchLocation)
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        let touch = touches.anyObject() as UITouch
+        let touchLocation = touch.locationInNode(self)
+        sceneTouched(touchLocation)
+    }
 }
